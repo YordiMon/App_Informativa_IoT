68a60a926b8ba18784b0fd15b59b646a16a299fe
YORDI <yordiazaelmzmzgmail.com>
2024-04-17 10:12:29 -0700

    Se creó la aplicación en base a la plantilla Empty Views Activity

---
+++ app/src/main/java/com/yamz/infoiot/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.yamz.infoiot
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
