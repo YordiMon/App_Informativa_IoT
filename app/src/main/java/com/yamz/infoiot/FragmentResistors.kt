74b32c400cab62ebb20c77a2fb8d291f85261b2b
JuanCarlosBL <146831092+JuanCarlosBL@users.noreply.github.com>
2024-04-18 19:53:01 -0700

    todos los fragmentos sin la navegacion

---
+++ app/src/main/java/com/yamz/infoiot/FragmentResistors.kt
@@ -0,0 +1,14 @@
+package com.yamz.infoiot
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+
+class FragmentResistors : Fragment() {
+
+}
